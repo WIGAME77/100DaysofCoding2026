637c687eb814244ae68f8e3688472887cadf21f7
WIGAME77 <wigame127@gmail.com>
2026-09-02 21:47:00 +0800

    Day1.java

---
+++ Day1.java
@@ -0,0 +1,16 @@
+public class Day1{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Ini adalah komentar satu baru
+/* 
+  Ini adalah komentar multi-baris.
+  Bisa ditulis lebih dari satu baris.
+*/
+
+/**
+ * Ini adalah komentar dokumentasi
+ * untuk menjelaskan fungsi kelas atau metode.
+ */
+    }
+}
