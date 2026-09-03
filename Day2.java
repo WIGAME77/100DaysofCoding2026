9c9c1e5fbbf053bc1f3ecc6cdb7f5951f59090cc
WIGAME77 <wigame127@gmail.com>
2026-09-03 19:30:25 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,22 @@
+public class Day2 {
+    public static void main(String[] args) {
+        System.out.println("=== BIODATA DIRI ===");
+        System.out.println("----------------------------------------");
+
+        System.out.print("Nama Lengkap : ");
+        System.out.println("Adid Fahriansyah");
+
+        System.out.print("Alamat       : ");
+        System.out.println("Mamuju");
+
+        int umur = 19;
+        double tinggiBadan = 160.9;
+        char jenisKelamin = 'L';
+
+        System.out.printf("Jenis Kelamin: %c%n", jenisKelamin);
+        System.out.printf("Usia         : %d tahun%n", umur);
+        System.out.printf("Tinggi Badan : %.1f cm%n", tinggiBadan);
+
+        System.out.println("----------------------------------------");
+    }
+}
