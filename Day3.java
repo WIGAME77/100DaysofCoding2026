8e3fb13af30ead5edda3bc530fd442a47e3f5003
WIGAME77 <wigame127@gmail.com>
2026-09-04 22:11:13 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,16 @@
+public class Day3{
+    public static void main(String[] args) {
+        System.out.println("Ini baris pertama.\nIni baris kedua.\n");
+        
+        System.out.println("Biodata Singkat:");
+        System.out.println("Nama\t\t: asep");
+        System.out.println("Profesi \t: pengangguran");
+        System.out.println("Kota\t\t: Konoha\n");
+        
+        System.out.println("asep berkata dengan semangat, \"\"Saya bangga jadi pengangguran\"\"");
+        
+        System.out.println("Tipe data karakter yang menyimpan nilai \'A\' adalah char.");
+        
+        System.out.println("Saya menyimpan file java di: C:\\vscode\\100DaysofCoding");
+    }
+}
