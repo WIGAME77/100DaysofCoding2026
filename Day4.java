5ea2358a381442e89818ccf049a73960d2287d10
WIGAME77 <wigame127@gmail.com>
2026-09-05 12:50:51 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,18 @@
+public class Day4 {
+    public static void main(String[] args) {
+        
+        byte umur = 26;
+        
+        short tahunLahir = 2000;
+        
+        int hargaLaptop = 12500000;
+        
+        long hutangNegara = 7000000000000L;
+        
+        System.out.println("=== DATA ANGKA BULAT ===");
+        System.out.println("Umur         : " + umur + " tahun");
+        System.out.println("Tahun Lahir  : " + tahunLahir);
+        System.out.println("Harga Laptop : Rp " + hargaLaptop);
+        System.out.println("Hutang Negara: Rp " + hutangNegara);
+    }
+}
