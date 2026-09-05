2197a36565b52e161e06c8f515c5fad7000da2ce
WIGAME77 <wigame127@gmail.com>
2026-09-06 01:02:44 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,11 @@
+public class Day5 {
+    public static void main(String[] args) {
+       
+        float suhuRuangan = 24.3f;
+        double nilai = 3.141592653589793;
+       
+        System.out.println("=== ANGKA PECAHAN ===");
+        System.out.println("Suhu Ruangan : " + suhuRuangan + " derajat");
+        System.out.println("Nilai        : " + nilai);
+    }
+}
