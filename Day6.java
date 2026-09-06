34d28b3ebbaa77913d8494ea517c5547cc2b4cb3
WIGAME77 <wigame127@gmail.com>
2026-09-07 00:02:10 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,9 @@
+public class Day6 {
+    public static void main(String[] args) {
+        char a = 'A';
+        boolean b = true;
+
+        System.out.println("ini char\t:"+a);
+        System.out.println("ini boolean\t: "+b);
+    }
+}
