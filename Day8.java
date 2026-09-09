702fb6947b3d36b3ca649d8ac3e1889e23d149cd
WIGAME <wigame127@gmail.com>
2026-09-09 19:59:49 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,26 @@
+public class Day8 {
+    public static void main(String[] args) {
+        int a;
+        double b;
+
+        a = 30;
+        b = 12.4;
+
+        int c = 100;
+
+        System.out.println("nilai1 : " + a);
+        System.out.println("nilai2 : " + b);
+        System.out.println("nilai3 : " + c);
+
+        a = 40;
+        b = 14.5;
+
+        c = c + 50;
+
+        System.out.println("\nnilai11 : " + a);
+        System.out.println("nilai22 : " + b);
+        System.out.println("nilai33 : "  + c);
+
+
+    }
+}
