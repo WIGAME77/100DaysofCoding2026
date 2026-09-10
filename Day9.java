a2f688139afdb42669908006cce56da6020a1388
WIGAME <wigame127@gmail.com>
2026-09-10 19:00:25 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,13 @@
+public class Day9 {
+    public static void main(String[] args) {
+        final int a = 77;
+        int b = 27;
+        System.out.println(a);
+        System.out.println(b);
+
+        // a = 46;
+        System.out.println("this thing cannot be executed bcs variable a is final");
+        b = 30;
+        System.out.println(b);
+    }
+}
