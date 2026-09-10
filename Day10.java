b2d84fc6b78d624ea217fd68b07afe29c41906eb
WIGAME <wigame127@gmail.com>
2026-09-11 04:05:20 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,10 @@
+public class Day10 {
+    public static void main(String[] args) {
+        System.out.println("Batas Byte   : " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+        System.out.println("Batas Short  : " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+        System.out.println("Batas Int    : " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+        System.out.println("Batas Long   : " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+        System.out.println("Batas Float  : " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+        System.out.println("Batas Double : " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+    }
+}
