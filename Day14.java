40de6438469d585b614dca9af64298f5820bb61a
WIGAME <wigame127@gmail.com>
2026-09-15 07:22:23 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,10 @@
+public class Day14 {
+    public static void main(String[] args) {
+        int a = 77;
+        int b = 46;
+        int c = a + b;
+        System.out.println("The hasil penjumlahan: " + c);
+        int d = a - b;
+        System.out.println("The hasil pengurangan: " + d);
+    }
+}
