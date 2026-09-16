e4c08179c617876573e83fb39273a62331ddd6fc
WIGAME <wigame127@gmail.com>
2026-09-16 20:43:55 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,11 @@
+public class Day15 {
+    public static void main(String[] args){
+        int a = 77;
+        int b = 46;
+        int c = a * b;
+        System.out.println(a+" × "+b+" = "+ c);
+        int d = a / b;
+        System.out.println(a +" ÷ "+b+" = " + d);
+    }
+    
+}
