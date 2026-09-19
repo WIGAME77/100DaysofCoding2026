6e0735c48d0d57c3d54360484e4125abb4d54536
WIGAME <wigame127@gmail.com>
2026-09-19 22:25:20 +0800

    Day18

---
+++ Day18.java
@@ -0,0 +1,17 @@
+public class Day18 {
+    public static void main(String[] args) {
+        byte b = 25;
+        short bs = b;
+        int bi = bs;
+        long bl = bi;
+        float bf = bl;
+        double bd = bf;
+        
+        System.out.println("Tipe byte\t: " + b);
+        System.out.println("Tipe short\t: " + bs);
+        System.out.println("Tipe int\t: " + bi);
+        System.out.println("Tipe long\t: " + bl);
+        System.out.println("Tipe float\t: " + bf);
+        System.out.println("Tipe double\t: " + bd);
+    }
+}
